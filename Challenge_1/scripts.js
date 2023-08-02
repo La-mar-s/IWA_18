c59b3b4c746471e1fe31d117baf19695cfeb8154
La-mar-s <sony.lamar.1995@gmail.com>
2023-08-02 11:44:30 +0200

    i do not know how to fix the issue with the rest of the code

--- Challenge_1/scripts.js
+++ Challenge_1/scripts.js
@@ -28,14 +28,93 @@ const handleDragOver = (event) => {
 }
 
 
-const handleDragStart = (event) => {}
-const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {}
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
-const handleEditSubmit = (event) => {}
-const handleDelete = (event) => {}
+const handleDragStart = (event) => {
+  const orderId = event.target.dataset.id;
+  updateDragging({ source: orderId });
+};
+
+const handleDragEnd = (event) => {
+  updateDragging({ source: null });
+  updateDraggingHtml({ over: null });
+};
+
+const handleHelpToggle = (event) => {
+  html.help.overlay.classList.toggle("visible");
+  if (html.help.overlay.classList.contains("visible")) {
+    html.help.overlay.focus();
+  } else {
+    html.other.add.focus();
+  }
+};
+
+const handleAddToggle = (event) => {
+  if (html.add.overlay.classList.contains("visible")) {
+    html.add.overlay.classList.remove("visible");
+    html.add.title.value = "";
+    html.add.table.selectedIndex = 0;
+  } else {
+    html.add.overlay.classList.add("visible");
+    html.add.title.focus();
+  }
+};
+
+const handleAddSubmit = (event) => {
+  event.preventDefault();
+  const title = html.add.title.value.trim();
+  const table = html.add.table.value;
+
+  if (title && table) {
+    const order = createOrderData({ title, table, column: "ordered" });
+    state.orders[order.id] = order;
+    html.columns.ordered.appendChild(createOrderHtml(order));
+    handleAddToggle();
+  }
+};
+
+const handleEditToggle = (event) => {
+  const orderId = event.target.dataset.id;
+  if (orderId) {
+    const order = state.orders[orderId];
+    html.edit.title.value = order.title;
+    html.edit.table.value = order.table;
+    html.edit.column.value = order.column;
+    html.edit.id.value = order.id;
+    html.edit.overlay.classList.add("visible");
+    html.edit.title.focus();
+  }
+};
+
+const handleEditSubmit = (event) => {
+  event.preventDefault();
+  const orderId = html.edit.id.value;
+  const title = html.edit.title.value.trim();
+  const table = html.edit.table.value;
+  const column = html.edit.column.value;
+
+  if (title && table && column && orderId) {
+    const order = state.orders[orderId];
+    order.title = title;
+    order.table = table;
+    order.column = column;
+
+    const orderHtml = document.querySelector(`[data-id="${orderId}"]`);
+    orderHtml.querySelector("[data-order-title]").textContent = title;
+    orderHtml.querySelector("[data-order-table]").textContent = table;
+
+    handleEditToggle();
+  }
+};
+
+const handleDelete = (event) => {
+  const orderId = html.edit.id.value;
+  if (orderId && confirm("Are you sure you want to delete this order?")) {
+    const orderHtml = document.querySelector(`[data-id="${orderId}"]`);
+    orderHtml.remove();
+    delete state.orders[orderId];
+    handleEditToggle();
+  }
+};
+
 
 html.add.cancel.addEventListener('click', handleAddToggle)
 html.other.add.addEventListener('click', handleAddToggle)
